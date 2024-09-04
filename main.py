3769e265d60a824cbd58577c4d896aee471b01ca
Dima <dima@emeraldcloudlab.com>
2024-09-04 10:30:55 -0400

    add asteroids and field, move to use groups for updates and draw calls

--- main.py
+++ main.py
@@ -4,22 +4,39 @@
 import pygame
 from constants import *
 from player import *
+from asteroid import *
+from asteroidfield import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     game_clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids,updatable,drawable)
+    AsteroidField.containers = (updatable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
+
     dt = 0
-    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill("#000000")
 
-        player.update(dt)
-        player.draw(screen)
+        for item in updatable:
+            item.update(dt)
+
+        screen.fill("black")
+
+        for item in drawable:
+            item.draw(screen)
 
         # render the updates
         pygame.display.flip()
--- player.py
+++ player.py
@@ -2,6 +2,8 @@ from circleshape import *
 from constants import *
 
 class Player(CircleShape):
+    containers = ()
+
     def __init__(self,x,y):
         super().__init__(x,y,PLAYER_RADIUS)
         self.rotation = 0
